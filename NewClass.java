63e75bb150e537e47969b2231f4e8242617e9827
Connor Brake <153332456+ConnorBrake@users.noreply.github.com>
2023-12-11 11:07:08 -0500

    Update NewClass.java

--- NewClass.java
+++ NewClass.java
@@ -1,14 +1,6 @@
 package ght;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author CoBra1341
- */
 public class NewClass {
     public static void main(String[]args)
     {
